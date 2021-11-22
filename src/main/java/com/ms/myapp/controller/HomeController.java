d5dcee74565e77b1cde36b30571b29a17f893e94
vahidmohsseni <vahidmohsseni@yahoo.com>
2021-11-22 21:37:19 +0200

    Add static index.

---
+++ src/main/java/com/ms/myapp/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.ms.myapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@RestController
+@RequestMapping(value = "/")
+@RequiredArgsConstructor
+public class HomeController {
+
+    private static String indexPage = "<!DOCTYPE html>\n" + "<html>\n" + "<body>\n" + "\n" + "<h1>Welcome!</h1>\n" + "\n" + "<p>Go to <a href='http://localhost:8080/countries/'>http://localhost:8080/countries/</a> page.</p>\n" + "<p>For More information refer <a target=_blank href='https://github.com/vahidmohsseni/CountryService'>here</a>!</p>" + "\n" + "</body>\n" + "</html>";
+
+
+    @GetMapping(value = {"", "/"})
+    private String homePage(){
+        return indexPage;
+    }
+
+}
